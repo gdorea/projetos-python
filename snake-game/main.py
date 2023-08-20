ef4809b529521b6dce55ccdddb966b7023d1bc0e
Gerzianni Dórea <gerzii@hotmail.com>
2023-08-20 19:13:34 -0300

    snake game food

--- snake-game/main.py
+++ snake-game/main.py
@@ -1,6 +1,8 @@
-from turtle import Screen, Turtle
-from snake import Snake
+from turtle import Screen
 import time
+from snake import Snake
+from food import Food
+
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -9,6 +11,8 @@ screen.title("My Snake Game")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
+
 screen.listen()
 screen.onkey(snake.up, "Up")
 screen.onkey(snake.down, "Down")
@@ -21,6 +25,9 @@ while game_in_on:
     screen.update()
     time.sleep(0.1)
     snake.move()
+
+    if snake.head.distance(food) < 15:
+        food.refresh()
 
 
 screen.exitonclick()
