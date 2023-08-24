4c7e06c22da592816bd4dc87e489ea3859cc95f5
Gerzianni Dórea <gerzii@hotmail.com>
2023-08-23 21:29:40 -0300

    pong game paddle

--- pong-game/main.py
+++ pong-game/main.py
@@ -1,9 +1,15 @@
-from turtle import Screen
+from turtle import Screen, Turtle
 
 screen = Screen()
 
 screen.setup(width=800, height=600)
 screen.bgcolor("black")
 screen.title("Pong Game")
+
+paddle = Turtle()
+paddle.shape("square")
+paddle.color("white")
+paddle.shapesize(stretch_wid=20, stretch_len=100)
+             
 
 screen.exitonclick()
