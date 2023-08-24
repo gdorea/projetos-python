b0d1bb590f092a4886f5f1fc53eaafff98200ddf
Gerzianni Dórea <gerzii@hotmail.com>
2023-08-24 18:05:52 -0300

    paddle class

--- pong-game/main.py
+++ pong-game/main.py
@@ -1,15 +1,24 @@
-from turtle import Screen, Turtle
+from turtle import Screen
+from paddle import Paddle
 
 screen = Screen()
 
 screen.setup(width=800, height=600)
 screen.bgcolor("black")
 screen.title("Pong Game")
+screen.tracer(0)
 
-paddle = Turtle()
-paddle.shape("square")
-paddle.color("white")
-paddle.shapesize(stretch_wid=20, stretch_len=100)
-             
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
+
+screen.listen()
+screen.onkey(r_paddle.go_up, 'Up')
+screen.onkey(r_paddle.go_down, 'Down')
+screen.onkey(l_paddle.go_up, 'w')
+screen.onkey(l_paddle.go_down, 's')
+
+game_is_on = True
+while game_is_on:
+    screen.update()
 
 screen.exitonclick()
