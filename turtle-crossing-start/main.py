84229451d3d08db3492134252f503e615a5ba602
Gerzianni Dórea <gerzii@hotmail.com>
2023-08-26 18:01:18 -0300

    turtle crossing classes

--- turtle-crossing-start/main.py
+++ turtle-crossing-start/main.py
@@ -8,7 +8,27 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 
+turtle = Player()
+car_manager = CarManager()
+
+screen.listen()
+screen.onkey(turtle.move, "Up")
+
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+
+    car_manager.create_car()
+    car_manager.move_cars()
+
+    for car in car_manager.all_cars:
+        if car.distance(turtle) < 20:
+            game_is_on = False
+
+    if turtle.is_at_finish_line():
+        turtle.go_to_start()
+        car_manager.level_up()
+
+
+screen.exitonclick()
