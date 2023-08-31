8ff937ce6da725c4cb11dcd0f11c6140f89a10e5
Gerzianni Dórea <gerzii@hotmail.com>
2023-08-31 09:03:07 -0300

    abs and rel paths

--- day-24/main.py
+++ day-24/main.py
@@ -3,9 +3,18 @@
 #print(contents)
 #file.close()
 
-with open("day-24/my_file.txt") as file:
+#with open("day-24/my_file.txt") as file:
+#    contents = file.read()
+#    print(contents)
+
+#with open("day-24/my_file.txt", mode="a") as file:
+#    file.write("\nNew text.")
+
+#with open("/Users/gerzi/Desktop/my_file.txt") as file: #absolute path
+#    contents = file.read()
+#    print(contents)
+
+
+with open("../../../../gerzi/Desktop/my_file.txt") as file: #relative path o ../ é usado para voltar uma pasta.
     contents = file.read()
     print(contents)
-
-with open("day-24/my_file.txt", mode="a") as file:
-    file.write("\nNew text.")
