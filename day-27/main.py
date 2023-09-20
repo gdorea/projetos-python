7b5638b9d2ceb441dc6a87187e1283aee8f4c5c7
Gerzianni Dórea <gerzii@hotmail.com>
2023-09-19 22:09:22 -0300

    day 27

--- day-27/main.py
+++ day-27/main.py
@@ -9,6 +9,18 @@ window.minsize(width=500, height=300)
 my_label = tkinter.Label(text="I am a Label", font=("Arial", 24, "bold"))
 my_label.pack()
 
+my_label.config(text="New Text")
 
+#Button
+
+def button_clicked():
+    #my_label.config(text="Button Got Clicked")
+    my_label.config(text=input.get())
+
+button = tkinter.Button(text="Click Me", command=button_clicked)
+button.pack()
+
+input = tkinter.Entry()
+input.pack()
 
 window.mainloop()
