c8729ad63d51b97a84720a39d538792a8a53805f
Gerzianni Dórea <gerzii@hotmail.com>
2023-09-25 21:19:24 -0300

    pomodoro final

--- pomodoro-start/main.py
+++ pomodoro-start/main.py
@@ -11,9 +11,18 @@ WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 reps = 0
+timer = None
 
 # ---------------------------- TIMER RESET ------------------------------- # 
 
+def reset_timer():
+    window.after_cancel(timer)
+    canvas.itemconfig(timer_text, text="00:00")
+    timer.config(text="Timer")
+    check.config(text="")
+    global reps
+    reps = 0
+    
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 def start_timer():
     global reps
@@ -43,10 +52,15 @@ def count_down(count):
         count_sec = f"0{count_sec}"
     canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
     if count > 0:
-        window.after(1000, count_down, count - 1)
+        global timer
+        timer = window.after(1000, count_down, count - 1)
     else:
         start_timer()
-
+        mark = ""
+        work_sessions = math.floor(int(reps/2))
+        for n in range(work_sessions):
+            mark += "✔"
+        check.config(text=mark)
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -68,10 +82,10 @@ canvas.grid(column=1, row=1)
 start_button = Button(text="Start", highlightthickness=0, command=start_timer)
 start_button.grid(column=0, row=2)
 
-reset_button = Button(text="Reset", highlightthickness=0)
+reset_button = Button(text="Reset", highlightthickness=0, command=reset_timer)
 reset_button.grid(column=2, row=2)
 
-check = Label(text="✔", fg=GREEN, bg=YELLOW)
+check = Label(fg=GREEN, bg=YELLOW)
 check.grid(column=1, row=3)
 
 
