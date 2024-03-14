258fb8f498e843a7dc37fa98ad882199b4f0a021
Gerzianni Fontes <gerzii@hotmail.com>
2024-03-14 19:54:26 -0300

    kanye quotes

--- day-33/main.py
+++ day-33/main.py
@@ -1,5 +1,13 @@
 import requests
 
 response = requests.get("http://api.open-notify.org/iss-now.json")
+response.raise_for_status()
 
-print(response)
+data = response.json()
+
+longitude = data["iss_position"]["longitude"]
+latitude = data["iss_position"]["latitude"]
+
+iss_position = (longitude, latitude)
+
+print(iss_position)
