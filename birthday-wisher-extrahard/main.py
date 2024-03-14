0d9a0252664124009216ba42658c89368687b027
Gerzianni Fontes <gerzii@hotmail.com>
2024-03-14 11:19:35 -0300

    day-33

--- birthday-wisher-extrahard/main.py
+++ birthday-wisher-extrahard/main.py
@@ -1,14 +1,24 @@
-##################### Extra Hard Starting Project ######################
+from datetime import datetime
+import pandas
+import random
+import smtplib
 
-# 1. Update the birthdays.csv
+MY_EMAIL = "gerzianniteste@gmail.com"
+MY_PASSWORD = "fxriivcbcaylrtzz"
 
-# 2. Check if today matches a birthday in the birthdays.csv
+today = datetime.now()
+today_tuple = (today.month, today.day)
 
-# 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
+data = pandas.read_csv("./projetos-python/birthday-wisher-extrahard/birthdays.csv")
+birthdays_dict = {(data_row["month"], data_row["day"]): data_row for (index, data_row) in data.iterrows()}
+if today_tuple in birthdays_dict:
+    birthday_person = birthdays_dict[today_tuple]
+    file_path = f"./projetos-python/birthday-wisher-extrahard/letter_templates/letter_{random.randint(1, 3)}.txt"
+    with open(file_path) as letter_file:
+        contents = letter_file.read()
+        contents = contents.replace("[NAME]", birthday_person["name"])
 
-# 4. Send the letter generated in step 3 to that person's email address.
-
-
-
-
-print("hello world")
+with smtplib.SMTP("smtp.gmail.com") as connection:
+    connection.starttls()
+    connection.login(MY_EMAIL, MY_PASSWORD)
+    connection.sendmail(from_addr=MY_EMAIL, to_addrs=birthday_person["email"], msg=f"Subject: Happy Birthday!\n\n {contents}")
