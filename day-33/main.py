6bdb457ed668eb4533f3f68055797b2e6bf1ee9a
Gerzianni Fontes <gerzii@hotmail.com>
2024-03-20 18:17:07 -0300

    day 33 final

--- day-33/main.py
+++ day-33/main.py
@@ -1,34 +1,50 @@
 import requests
 from datetime import datetime
+import smtplib
+import time
 
+my_email = "gerzianniteste@gmail.com"
+password = "fxriivcbcaylrtzz"
 MY_LAT = -10.947247
 MY_LONG = -37.073082
 
-#response = requests.get("http://api.open-notify.org/iss-now.json")
-#response.raise_for_status()
+def is_iss_overhead():
+    response = requests.get("http://api.open-notify.org/iss-now.json")
+    response.raise_for_status()
+    data = response.json()
 
-#data = response.json()
+    iss_longitude = float(data["iss_position"]["longitude"])
+    iss_latitude = float(data["iss_position"]["latitude"])
 
-#longitude = data["iss_position"]["longitude"]
-#latitude = data["iss_position"]["latitude"]
+    if MY_LONG - 5 <= iss_longitude <= MY_LONG + 5 and MY_LAT - 5 <= iss_latitude <= MY_LAT + 5:
+        return True
 
-#iss_position = (longitude, latitude)
 
-parameters = {
-    "lat": MY_LAT,
-    "lng": MY_LONG,
-    "formatted": 0,
-}
+def is_night():
+    parameters = {
+        "lat": MY_LAT,
+        "lng": MY_LONG,
+        "formatted": 0,
+    }
+    response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+    response.raise_for_status()
+    data = response.json()
+    sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+    sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0]) 
 
-response = requests.get("https://api.sunrise-sunset.org/json", params=parameters, )
-response.raise_for_status()
-data = response.json()
-sunrise = data["results"]["sunrise"].split("T")[1].split(":")[0]
-sunset = data["results"]["sunset"].split("T")[1].split(":")[0]
+    time_now = datetime.now().hour
 
-print(sunrise)
-print(sunset)
-
-time_now = datetime.now()
-
-print(time_now.hour)
+    if time_now >= sunset or time_now <= sunrise:
+        return True
+    
+while True:
+    time.sleep(60)
+    if is_iss_overhead() and is_night():
+        with smtplib.SMTP("smtp.gmail.com") as connection:
+            connection.starttls()
+            connection.login(user=my_email, password=password)
+            connection.sendmail(
+                from_addr=my_email,
+                to_addrs="gerzii@hotmail.com",
+                msg="Subject:Look at the sky!\n\nThe ISS is above you in the sky!"
+            )
