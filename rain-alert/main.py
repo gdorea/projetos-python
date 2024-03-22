f12c8c381ce6d10aeab5f70b0ce7c271322f5f13
Gerzianni Fontes <gerzii@hotmail.com>
2024-03-21 21:12:48 -0300

    rain alert start

---
+++ rain-alert/main.py
@@ -0,0 +1,24 @@
+import requests
+
+api_key = "5791e486f3436f2600f0f91957f9246a"
+
+parameters = {
+    "lat": -7.192070,
+    "lon": -48.207790,
+    "appid": api_key,
+    "cnt": 4,
+}
+
+response = requests.get("https://api.openweathermap.org/data/2.5/forecast", params=parameters)
+response.raise_for_status()
+weather_data = response.json()
+
+#print(weather_data["list"][0]["weather"][0]["id"])
+
+will_rain = False
+for hour_data in weather_data["list"]:
+    condition_code = hour_data["weather"][0]["id"]
+    if int(condition_code) < 700:
+        will_rain = True
+if will_rain:
+    print("Bring an umbrella.")
